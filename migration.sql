1b24bed61f65281d052a3ac954d775a44bcdacfd
Yassine Raddadi <yassine.raddadi.4@gmail.com>
2026-02-23 23:44:16 +0100

    clearing main for update

--- migration.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS gamification (
-    user_id INT PRIMARY KEY,
-    xp INT DEFAULT 0,
-    level INT DEFAULT 1,
-    title VARCHAR(50) DEFAULT 'Novice',
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
